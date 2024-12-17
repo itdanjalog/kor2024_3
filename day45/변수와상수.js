5466e14c7dc05fbc5b1332284e8525b6c2e83038
itdanjalog <kgs2072@naver.com>
2024-12-17 20:49:11 +0900

    day45

--- day45/변수와상수.js
+++ day45/변수와상수.js
@@ -24,7 +24,41 @@ console.log( value4 );  // 유재석
 const value5 = value4 + `님 안녕하세요.`;
 console.log( value5 );
 
-// 실습1 : prompt() 함수로 하나의 점수를 입력받아서 console.log() 함수로 console탭에 입력받은 점수를 출력하시오.
+// 실습1 : 
+// prompt() 함수로 하나의 점수를 입력받아서 (변수에 저장한후) 
+// console.log() 함수로 console탭에 입력받은 점수를 출력하시오.
+
+// prompt(`[실습1] 점수 입력하세요.`)  // (1) 입력받은 값을 반환 하는 함수
+// let score1 = prompt( `[실습1] 점수 입력하세요.` ); // (1) 입력받은 값을 반환받아 (2) 변수에 대입
+
+let score1 = prompt( `[실습1] 점수 입력하세요.` ); // (1) 입력받은 값을 반환받아 (2) 변수에 대입
+console.log( score1 ) // (3) 출력함수에 출력할 변수명(상징적인 이름) 작성하여 변수가 갖는 값 호출한다.
+// * 컴퓨터 기본적인 프로세스(절차) : 1.입력 2.처리 3.출력
+
+// 실습2 : 
+// prompt() 함수로 국어점수 와 수학점수를 각각 입력받아서 (변수에 저장한후)
+// console.log() 함수에 총점 과 평균을 각 출력하시오.
+
+// [1] 입력 
+let korScore = Number( prompt(`[실습2] 국어점수 입력하세요.`) ) ;
+let mathScore =  Number( prompt(`[실습2] 수학점수 입력하세요.`) ) ;
+// [2] 처리 
+let sum = korScore + mathScore ; 
+let avg = sum / 2 ; 
+// [3] 출력 
+console.log( sum );
+console.log( avg );
+
+
+
+
+
+
+
+
+
+
+
 
 
 
