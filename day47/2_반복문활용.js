066375d4dfd8dcc3cf5354c2d2a302b056050902
itdanjalog <kgs2072@naver.com>
2024-12-19 21:21:41 +0900

    day47

--- day47/2_반복문활용.js
+++ day47/2_반복문활용.js
@@ -1,3 +1,15 @@
+/*
+[문제1] 아래 배열을 이용한 영화 평점 출력 구현하기.
+1. 영화이름 목록 , 영화이름과 영화평점은 동일한 인덱스를 사용한다.
+    - movieNames = [ '히든페이스' , '위키드' , '글래디에이터2' , '청설' ]
+2. 영화 평점 목록 , 각 영화별 평점은 10점 만점
+    - movieRating = [ 8 , 4 , 7 , 6 ]
+3. HTML 예시 
+    히든페이스     	    ★★★★★★★★☆☆
+    위키드              ★★★★☆☆☆☆☆☆
+    글래디에이터2        ★★★★★★★☆☆☆
+    청설                ★★★★★★☆☆☆☆
+*/
 
 // [문제1 구현]
     // (1) 영화의 제목과 영화 평점을 배열로 선언 
@@ -32,5 +44,40 @@ console.log( outHTML )
     // (3) DOM객체를 이용한 html에 변수값 출력하기 
 let exampleDIV1객체 = document.querySelector('#exampleDIV1')    // 특정한 선택자의 마크업을 JS객체로 가져온다. 
 exampleDIV1객체.innerHTML = outHTML; // 가져온 마크업 객체의 innerHTML를 이용하여 구성한 HTML를 대입한다.
+
+
+/*
+[문제2] 6개 좌석 상태 출력 구현하기.  
+1. 좌석은 총 6개 , 빈좌석인지 예약석인지는 6개 좌석 정보를 임의로 *배열* 구성 
+2. 아래와 같이 좌석를 2칸씩 3줄 출력한다.
+3. 빈좌석이면 color:blue 색상 표기 , 예약석이면 color:red 색상 표기 하시오.
+4. HTML 예시 
+    빈좌석 예약석   
+    예약석 빈좌석
+    예약석 빈좌석
+*/
+// (1) 좌석 상태를 저장하고 있는 배열 선언 
+let 좌석정보 = [ '빈좌석' , '예약석' , '예약석' , '빈좌석' , '예약석' , '빈좌석' ]
+
+// (2) HTML에 출력할 HTML문자열 구성하기.
+let seatHtml = ``;
+for( let index = 0 ; index <= 좌석정보.length-1 ; index++ ){
+    // - index는 0부터 마지막인덱스까지 1씩 증가 반복 , 6회
+    // index번째 값을 <span> 로 구성하기. +만약에 좌석정보가 '빈좌석'이면 'blue' 예약석이면 'red'
+    seatHtml += `<span style="color : ${ 좌석정보[index] == '빈좌석' ? 'blue' : 'red' } " > 
+                    ${ 좌석정보[index] } 
+                </span>`
+
+    // + 2배수 마다 줄바꿈 처리 
+    if( (index+1) % 2 == 0 ){ // 현재 인덱스 +1 했을때 2의 배수이면 
+        seatHtml += `<br/>` // 줄바꿈 
+    }
+
+}// for end 
+
+// (3) 특정 html 선택자로부터 마크업 객체를 가져와서 html 대입
+let exampleDIV2객체 = document.querySelector('#exampleDIV2')
+exampleDIV2객체.innerHTML = seatHtml
+
 
 
