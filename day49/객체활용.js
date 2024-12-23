1e3d8f10186c3814b5c8e3a2f515e37c050313c3
itdanjalog <kgs2072@naver.com>
2024-12-23 21:32:44 +0900

    day49

--- day49/객체활용.js
+++ day49/객체활용.js
@@ -48,6 +48,8 @@ function signUp(){ // 함수란? 복잡한 코드를 집합{}해서 재사용
     signpw.value = '';
     signnic.value = '';
 
+    return;
+
 } // f end 
 
 // [ 2. 로그인 기능 함수 구현 ]
@@ -57,4 +59,35 @@ function signUp(){ // 함수란? 복잡한 코드를 집합{}해서 재사용
     // 매개변수 : X 
     //      HTML( id , pw )는 document.querySelector()
     // 리턴값 : X 
-    //      HTML( 성공/실패 메시지 )는 alert , innerHTML 
+    //      HTML( 성공/실패 메시지 )는 alert , innerHTML
+function login(){
+    console.log( 'login() 실행'); // HTML에서 함수를 호출하는 방법 <마크업 onclick="함수명()" />
+    // [1] HTML로부터 입력받은 값 가져오기 위해 INPUT 마크업 객체 가져온다.
+        // (왜?) DOM객체가 필요할까? html는 단순 문자열 언어이므로 , 함수/변수 사용할수없다.
+        // 아 그래서 JS가 HTML 마크업들을 객체화 해서 마크업을 객체처럼 다루기 위한 객체 제공. 
+    let loginid = document.querySelector('.loginid'); 
+    let loginpw = document.querySelector('.loginpw');
+    // [2] input 마크업 객체로 부터 value 속성 가져오기 
+    let id = loginid.value;
+    let pw = loginpw.value;
+    // [3] 회원목록 (DB) 배열에서 입력받은 값 찾기 
+    let loginState = false ; // 로그인 했다 안했다 판단 변수 : false:로그인실패 true:로그인성공 뜻으로 정의
+    for( let index = 0 ; index <= memberTable.length-1 ; index++ ){
+        // - index는 0부터 배열내 마지막인덱스까지 1씩증가 반복.
+        let memberDto = memberTable[index]; // index번째의 member객체 가져온다.
+        if( memberDto.id == id && memberDto.pw == pw ){
+            // 만약에 index번째 회원아이디와 입력한 아이디가 같고
+            // index번째 회원비밀번호와 입력한 비밀번호가 같으면 
+            loginState = true; // 로그인상태를 true 함으로써 로그인 성공 의미를 담는다.
+            break; // 반복문 종료.
+        }
+    } // for end 
+    // [4] 안내 메시지 
+    if( loginState == true ){  alert(`로그인 성공`);}
+    else{ alert('동일한 회원정보가 없습니다. ') }
+    loginid.value = ``;
+    loginpw.value = ``;
+    return;
+} // f end 
+
+
